9794b2aaa466409ac50bd5a0dbd8bd5e88cd6d68
Flavio Aguilar <flavio.red0568@gmail.com>
2026-09-13 13:42:03 -0600

    fix: reinicio scroll

--- js/main.js
+++ js/main.js
@@ -131,3 +131,13 @@ function cerrarModal() {
     modalDetalles.classList.remove('active');
     document.body.style.overflow = 'auto';
 }
+
+// LÓGICA PARA CERRAR EL MODAL REINICIANDO EL SCROLL
+const modalBody = document.querySelector('.modal-body');
+function cerrarModal() {
+    modalDetalles.classList.remove('active');
+    document.body.style.overflow = 'auto';
+    setTimeout(() => {
+        modalBody.scrollTop = 0; // Reinicia el scroll del modal al cerrar
+    }, 300); // Espera a que la animación de cierre termine
+}
